1e97922101477e4c34651c5386204e3e36f13231
T31K <t31kmunwong@gmail.com>
2023-03-18 15:24:34 +0800

    add dynamic download link

--- src/partials/Downloads.jsx
+++ src/partials/Downloads.jsx
@@ -1,19 +1,27 @@
-import React, { useState, useRef, useEffect } from 'react';
-import Transition from '../utils/Transition';
-import { Emoji } from 'react-apple-emojis';
-
-import FeaturesBg from '../images/features-bg.png';
-import FeaturesElement from '../images/features-element.png';
-
-import { FaRust } from 'react-icons/fa';
-import { SiElectron } from 'react-icons/si';
-import { FiPackage } from 'react-icons/fi';
-import { BsKeyboard } from 'react-icons/bs';
+import React, { useState, , useEffect } from 'react';
+import axios from 'axios'
 import { FaApple, FaLinux } from 'react-icons/fa';
 
-import IconBase from './IconBase';
 
 function Downloads() {
+  const [version, setVersion] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
+  
+  useEffect(() => {
+    getLatestVersion();
+  }, [])
+  
+  const getLatestVersion = async () => {
+    try {
+      const response = await axios.get(`https://api.github.com/repos/t31k/harmonize/releases/latest`);
+      const latestRelease = response.data;
+      const latestVersion = latestRelease.tag_name;
+      setVersion(latestVersion)
+      setIsLoading(false)
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <section className="relative pb-[100px]">
       <div className="relative max-w-6xl mx-auto px-4 sm:px-6 ">
@@ -30,11 +38,11 @@ function Downloads() {
             <div className="w-full lg:w-6/12 lg:pr-4">
               <div className="h-[250px] custom-container my-3">
                 <FaApple className="fa-icon"></FaApple>
-                <div className="text-lg font-semibold ml-1">Version 0.1.0-beta</div>
+                <div className="text-lg font-semibold ml-1">Version {version}</div>
                 <a
                   className="ml-1 mt-4 p-4 border-2 w-[150px] flex justify-center rounded-lg bg-gray-200 font-bold hover:cursor-pointer hover:bg-gray-300"
-                  href="https://github.com/T31K/harmonize-beta-release/raw/master/harmonize_0.1.1-beta.dmg"
-                >
+                  href={isLoading ? '#' : `https://github.com/T31K/harmonize/releases/download/${version}/harmonize_1.0.5_x64.dmg`}
+                  >
                   Download
                 </a>
               </div>
